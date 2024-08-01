6af8a6e2ebacba2705c9c80bd22b2694a2c30539
paulo_garcia <my.name@mapanacode.com>
2024-07-31 21:56:49 -0500

    [main] Add initial project configuration files and structure

---
+++ fe/src/components/HomeLanding.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive
+      class="align-centerfill-height mx-auto"
+      max-width="900"
+    >
+      <v-img
+        class="mb-4"
+        height="150"
+        src="@/assets/profile.jpeg"
+      />
+
+      <div class="text-center">
+        <div class="text-body-2 font-weight-light mb-n1">Soy</div>
+        <h1 class="text-h2 font-weight-bold">Paulo Andrés García</h1>
+      </div>
+      <div class="py-4" />
+      <v-row>
+        <v-col cols="12">
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
